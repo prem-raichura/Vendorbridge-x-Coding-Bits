c6e7d6f16a2e1a463614d292ce2199ac077ea8c1
Rohan Thakar <rpthakar35@gmail.com>
2026-06-06 15:36:49 +0530

    Manager and Vendor Signup succesfull

--- server/src/admin/features/manager/manager.routes.js
+++ server/src/admin/features/manager/manager.routes.js
@@ -3,8 +3,14 @@ const {
   createManager,
   deleteManager
 } = require('./manager.controller');
+const {
+  protect,
+  authorize
+} = require('../../../shared/gateway/apiGateway.middleware');
 
 const router = express.Router();
+
+router.use(protect, authorize('admin'));
 
 router.post('/', createManager);
 router.delete('/:user_id', deleteManager);
